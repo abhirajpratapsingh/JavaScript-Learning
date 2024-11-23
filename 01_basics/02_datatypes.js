97cb6037a7de0fcb76688b68228afb4d516d43c4
Abhiraj Pratap Singh <abhirajpratapsingh13@gmail.com>
2024-11-23 20:13:54 +0530

    DataTypes Document

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" ; // treat all js code as newer version 
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3);
+
+let name = "abhiraj" // string
+let age = 23    // number data type 
+let ishuman = true      // boolean data type
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => unassigned value
+// symbol => unique
+
+
+// object =>
+
+console.log( typeof("abhiraj")) ;
+// typeof is a method which tells the type of the data 
+
+console.log ( typeof 5 ) ; // we can also write like this
+
+console.log ( typeof null ) ;  // null is object type according to typeof
+
+console.log ( typeof undefined ) ; // it is undefined data type
