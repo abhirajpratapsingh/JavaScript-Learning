437f187826eda50e6854fdc27a502d78d1e53f4a
Abhiraj Pratap Singh <abhirajpratapsingh13@gmail.com>
2024-11-25 16:52:00 +0530

    Memory concept

--- 01_basics/DataTypes_Summary.js
+++ 01_basics/DataTypes_Summary.js
@@ -34,3 +34,24 @@ let myObj = {
 const myFunction = function(){
     console.log ("hello World") ;
 }
+
+
+//+++++++++++++++++++++++++++ Memory +++++++++++++++++++++++++++
+
+// Stack memory ( primitive ) sare primitive datatype ko stack me memory milti , yaani ki jo bhi hm variable pass krte h uska data copy hota h , or hm bhi hm value me changes krte h to copy vale me changes hote h original vale me nii 
+
+// Heap Memory ( Non-Primitive ) sare non-primitive datatype ko heap me memory milti hai , yaani ki jo bhi hm variable dete hai uska reference pass hota yani ki address or jb hm usme change krte h to original value bhi change ho jati h
+
+// Example of stack memory 
+let myname = "abhiraj"  // isko memory stack me mili h
+let newName = myname // isko bhi memory stack me mili h isme koi changes krege to myname me koi changes ni hoge
+newName = "abhiraj Pratap Singh"    // hmne iski value change kr di lekin myname me abhi bhi value vhi rhegi
+
+// Example of heap memory 
+let user = {   // isko memory heap me mili h to ab iska reference pass hoga
+    name : "abhiraj" ,
+    age : 23 
+}
+let user1 = user ;    // ab isme user ka reference aaya h to ab agr hm user1 me changes krege to user me bhi change ho jayega
+user1.age = 234 
+console.log ( user.age , user1.age ) ;
