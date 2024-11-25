afd8698a463685cfc11cbab2b9e411a7641d2adb
Abhiraj Pratap Singh <abhirajpratapsingh13@gmail.com>
2024-11-25 15:35:59 +0530

    comparision operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log ( 2 > 1 ) ;
+// console.log ( 2 >= 1 ) ;
+// console.log ( 2 < 1 ) ;
+// console.log ( 2 == 1 ) ;
+// console.log ( 2 != 1 ) ;
+// console.log ( 2 <= 1 ) ;
+
+
+console.log ( "2" > 1 ) ;       // true 
+console.log ( "02" > 1 ) ;      // true 
+
+console.log ( null > 0 ) ;      // false
+console.log ( null == 0 ) ;     // false 
+console.log ( null >= 0 ) ;     // true 
+
+console.log ( undefined == 0 )      // false
+console.log ( undefined > 0 )       // false 
+console.log ( undefined < 0 )       // false 
+
+// === ( it is strict check it checks value and datatype both ) 
+
+console.log ( "2" === 2 ) ;         // false
+console.log ( "2" == 2 ) ;          // true 
