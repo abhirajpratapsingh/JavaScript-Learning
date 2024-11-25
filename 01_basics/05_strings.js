0fc0cf8378b913c5740e7b1ba287f60527744683
Abhiraj Pratap Singh <abhirajpratapsingh13@gmail.com>
2024-11-25 17:42:52 +0530

    JavaScript learning

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "abhiraj" 
+const repoCount = 7 
+
+console.log ( name +" " + repoCount + " Value" ) ;
+console.log ( `Hello my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName = new String("AbhirajAps")
+console.log ( gameName ) ;          // here string is object 
+console.log ( typeof gameName ) ;
+// poori string key value pair me store hui h 
+console.log ( gameName[0] )
+console.log ( gameName[1] )
+console.log ( gameName[2] )
+console.log ( gameName[3] )
